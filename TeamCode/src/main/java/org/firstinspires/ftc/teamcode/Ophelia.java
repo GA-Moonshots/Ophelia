76ad88e2fe3704dc6df7e1bb7ee87346a7444eb1
Calpha <gabrielnsouto100@gmail.com>
2025-08-11 11:19:57 -0400

    Remove intake and lift subsystems and related commands
    Deleted all code related to the Intake and Lift subsystems, including their command classes and experiment utilities. Updated Ophelia.java to remove references to Intake and Lift, unregister them as subsystems, and remove all related button bindings. This refactor simplifies the robot code by removing unused or deprecated mechanisms.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ophelia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ophelia.java
@@ -17,13 +17,6 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.commands.Drive;
 import org.firstinspires.ftc.teamcode.commands.SplineToPose;
 import org.firstinspires.ftc.teamcode.commands.StrafeToPose;
-import org.firstinspires.ftc.teamcode.commands.intake.IntakeShoulderByPlayer;
-import org.firstinspires.ftc.teamcode.commands.intake.IntakeShoulderDown;
-import org.firstinspires.ftc.teamcode.commands.intake.TransferBlock;
-import org.firstinspires.ftc.teamcode.commands.lift.LiftLowerRTP;
-import org.firstinspires.ftc.teamcode.commands.lift.LiftRaiseRTP;
-import org.firstinspires.ftc.teamcode.subsystems.Intake;
-import org.firstinspires.ftc.teamcode.subsystems.Lift;
 import org.firstinspires.ftc.teamcode.subsystems.Mecanum;
 import org.firstinspires.ftc.teamcode.subsystems.SensorPackage;
 import org.firstinspires.ftc.teamcode.util.Constants;
@@ -31,7 +24,6 @@ import org.firstinspires.ftc.teamcode.util.experiments.AprilLimeDetect;
 import org.firstinspires.ftc.teamcode.util.experiments.ServoTest;
 
 public class Ophelia extends Robot {
-
     // INSTANCE VARIABLES
     public LinearOpMode opMode;
     public GamepadEx player1;
@@ -42,8 +34,6 @@ public class Ophelia extends Robot {
     // SUBSYSTEMS
     public Mecanum mecanum;
     public SensorPackage sensors;
-    public Lift lift;
-    public Intake intake;
 
     public Telemetry telemetry;
     public HardwareMap hardwareMap;
@@ -82,18 +72,15 @@ public class Ophelia extends Robot {
         // JUST FOR TESTING
         startOfTele = new Pose2d(new Vector2d(-36, -62), Math.toRadians(180));
         mecanum = new Mecanum(this, startOfTele);
-        lift = new Lift(this);
-        intake = new Intake(this);
         sensors = new SensorPackage(this);
         sensors.enableAprilTagTracking();
 
         // Register subsystems
         // REGISTER THE SUBSYSTEM BEFORE THE DEFAULT COMMANDS
-        register(mecanum, lift, intake, sensors);
+        register(mecanum, sensors);
 
         // Setting Default Commands. When not doing anything, respond to the controller
         mecanum.setDefaultCommand(new Drive(this));
-        intake.setDefaultCommand(new IntakeShoulderByPlayer(this));
 
         /*
 
@@ -153,66 +140,34 @@ public class Ophelia extends Robot {
 
         */
 
-        // BUTTON B -- DUMP BASKET
+        // BUTTON B -- NOTHING
         Button bButtonP2 = new GamepadButton(player2, GamepadKeys.Button.B);
-        bButtonP2.whenPressed(new InstantCommand(() -> {
-            lift.dumpBasket();
-        }));
 
-        // BUTTON Y -- LEVELS BASKET
+        // BUTTON Y -- Nothing
         Button yButtonP2 = new GamepadButton(player2, GamepadKeys.Button.Y);
-        yButtonP2.whenPressed(new InstantCommand(() -> {
-            lift.levelBasket();
-        }));
 
-        //  LEFT BUMPER -- transfer block to the basket
+        //  LEFT BUMPER -- NOTHING
         Button leftBumperP2 = new GamepadButton(player2, GamepadKeys.Button.LEFT_BUMPER);
-        leftBumperP2.whenPressed(new TransferBlock(this));
 
-        // LEFT TRIGGER -- SHOULDER DOWN
+        // LEFT TRIGGER -- NOTHING
         Trigger leftTriggerP2 = new Trigger(() -> player2.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.5);
-        leftTriggerP2.whenActive(new IntakeShoulderDown(this));
 
-        // DPAD DOWN -- LIFT LOWER
+        // DPAD DOWN -- NOTHING
         Button downDpadP2 = new GamepadButton(player2, GamepadKeys.Button.DPAD_DOWN);
-        downDpadP2.whenPressed(new LiftLowerRTP(this));
 
-
-        // DPAD UP -- LIFT RAISE
+        // DPAD UP -- NOTHING
         Button upDpadP2 = new GamepadButton(player2, GamepadKeys.Button.DPAD_UP);
-        upDpadP2.whenPressed(new SequentialCommandGroup(
-                // we always lower first to reset encoder
-                new LiftLowerRTP(this),
-                new LiftRaiseRTP(this, Constants.HIGH_HEIGHT)
-        ));
 
-        // RIGHT BUMPER -- NEGATIVE SPIN INTAKE
+        // RIGHT BUMPER -- NOTHING
         Button rightBumperP2 = new GamepadButton(player2, GamepadKeys.Button.RIGHT_BUMPER);
-        rightBumperP2.whenHeld(new InstantCommand(() -> {
-            intake.setSpinSpeed(Constants.INTAKE_SPIN_SPEED_BACK);
-        }));
-        // RIGHT BUMPER RELEASE -- SPIN STOP
-        rightBumperP2.whenReleased(new InstantCommand(() -> {
-            // these servo functions don't know when to stop, so we kill 'em on release
-            intake.setSpinSpeed(0);
-        }));
 
-        // RIGHT TRIGGER -- SPIN INTAKE
+        // RIGHT TRIGGER -- NOTHING
         Trigger rightTriggerP2 = new Trigger(() -> player2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.5);
-        rightTriggerP2.whenActive(new InstantCommand(() -> {
-            intake.setSpinSpeed(Constants.INTAKE_SPIN_SPEED_FORWARD);
-        }));
 
-        // RIGHT TRIGGER RELEASE -- SPIN STOP
-        rightTriggerP2.whenInactive(new InstantCommand(() -> {
-            // these servo functions don't know when to stop, so we kill 'em on release
-            intake.setSpinSpeed(0.0);
-        }));
-
-        // JOYSTICK LEFT - nothing
+        // JOYSTICK LEFT - NOTHING
         Button joyStickLeftTrigger = new GamepadButton(player2, GamepadKeys.Button.LEFT_STICK_BUTTON);
 
-        // JOYSTICK RIGHT - nothing
+        // JOYSTICK RIGHT - NOTHING
         Button joyStickRightTrigger = new GamepadButton(player2, GamepadKeys.Button.RIGHT_STICK_BUTTON);
     }
 
@@ -229,13 +184,11 @@ public class Ophelia extends Robot {
         }
 
         mecanum = new Mecanum(this, start);
-        lift = new Lift(this);
-        intake = new Intake(this);
         mecanum.makeFieldCentric();
         sensors = new SensorPackage(this);
         sensors.enableAprilTagTracking();
 
-        register(mecanum, lift, sensors);
+        register(mecanum, sensors);
 
         new SequentialCommandGroup(
                 new StrafeToPose(this, new Pose2d(-50, 0, 180))
